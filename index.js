25dd64b1eacb594344fa3b2389ff893a995d580f
Stefan-Gabriel Muscalu <stefan@findie.me>
2016-11-28 15:34:42 +0200

    Add: if no file is provided, UNIX pipe will be used

--- index.js
+++ index.js
@@ -3,67 +3,80 @@ const TIMING = !!process.env.TIMING;
 const sharp = require('sharp');
 const file = process.argv[2];
 
-if (!file) {
-  console.log("Please provide an image file");
-  process.exit(1);
-}
-
 TIMING && console.time('Done!');
 TIMING && console.time('Loading file');
 
-sharp(file)
-  .toColourspace('b-w')
-  .raw()
-  .toBuffer((err, raw, info) => {
-    if (err) {
-      console.error(err);
-      return process.exit(2);
-    }
-    TIMING && console.timeEnd('Loading file');
+const processSharp = (sharpLoad) => {
+  sharpLoad
+    .toColourspace('b-w')
+    .raw()
+    .toBuffer((err, raw, info) => {
+      if (err) {
+        console.error(err);
+        return process.exit(2);
+      }
+      TIMING && console.timeEnd('Loading file');
 
-    /**
-     * Explanation here:
-     *  http://www.astro.cornell.edu/research/projects/compression/entropy.html
-     */
+      /**
+       * Explanation here:
+       *  http://www.astro.cornell.edu/research/projects/compression/entropy.html
+       */
 
-    const h = info.height;
-    const w = info.width;
+      const h = info.height;
+      const w = info.width;
 
-    TIMING && console.time('Computing probabilities');
+      TIMING && console.time('Computing probabilities');
 
-    const probLen = 256 * 2 - 1;
-    const probabilities = new Uint32Array(probLen);
+      const probLen = 256 * 2 - 1;
+      const probabilities = new Uint32Array(probLen);
 
-    for (let i = 0; i < h; i++) {
-      for (let j = 0; j < w - 1; j++) {
-        const diff = raw[i * info.width + j + 1] - raw[i * info.width + j];
-        if (
-          diff < +(probLen + 1) / 2 &&
-          diff > -(probLen + 1) / 2
-        ) {
-          probabilities[diff + (probLen - 1) / 2]++;
+      for (let i = 0; i < h; i++) {
+        for (let j = 0; j < w - 1; j++) {
+          const diff = raw[i * info.width + j + 1] - raw[i * info.width + j];
+          if (
+            diff < +(probLen + 1) / 2 &&
+            diff > -(probLen + 1) / 2
+          ) {
+            probabilities[diff + (probLen - 1) / 2]++;
+          }
         }
       }
-    }
 
-    TIMING && console.timeEnd('Computing probabilities');
-    TIMING && console.time('Computing entropy');
+      TIMING && console.timeEnd('Computing probabilities');
+      TIMING && console.time('Computing entropy');
 
-    const total = probabilities.reduce((total, current) => total + current, 0);
-    let entropy = 0;
+      const total = probabilities.reduce((total, current) => total + current, 0);
+      let entropy = 0;
 
-    for (let i = 0; i < probLen; i++) {
-      const prob = probabilities[i] / total;
+      for (let i = 0; i < probLen; i++) {
+        const prob = probabilities[i] / total;
 
-      if (prob !== 0) {
-        entropy = entropy - prob * Math.log(prob)
+        if (prob !== 0) {
+          entropy = entropy - prob * Math.log(prob)
+        }
       }
-    }
 
-    entropy = entropy / Math.log(2);
+      entropy = entropy / Math.log(2);
 
-    TIMING && console.timeEnd('Computing entropy');
-    TIMING && console.timeEnd('Done!');
+      TIMING && console.timeEnd('Computing entropy');
+      TIMING && console.timeEnd('Done!');
 
-    console.log(entropy);
+      console.log(entropy);
+    });
+};
+
+if (file) {
+  processSharp(sharp(file))
+} else {
+  const stdin = global.process.openStdin();
+
+  let data = new Buffer(0);
+
+  stdin.on('data', function(chunk) {
+    data = Buffer.concat([data, new Buffer(chunk)]);
   });
+
+  stdin.once('end', function() {
+    processSharp(sharp(data));
+  });
+}
